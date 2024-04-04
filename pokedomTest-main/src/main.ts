88559f3994f12e527b895b3a19daeb57249027db
callum <callumgibbons@icloud.com>
2024-04-04 16:57:11 +0530

    continued attempt at creating search function.

--- pokedomTest-main/src/main.ts
+++ pokedomTest-main/src/main.ts
@@ -2,18 +2,25 @@ import { Pokemon } from "./data/types";
 import { pokemonArray } from "./data/pokemon";
 import "./styles/style.scss";
 
-const filterBySearch = (string: string) => {
+const pokemonSearch = document.querySelector<HTMLInputElement>(".pokemon_search");
 
-}
+
+const filterBySearch = () =>{ 
+    pokemonArray.filter(
+  (pokemonArray) => pokemonArray.name === (pokemonSearch?.toString())
+); }
 //this takes each string in a given array and converts the first letter of each item to capital
 const capitaliseFirstLetter = (string: string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
+console.log(filterBySearch);
+
+
 //created function for the making of each card, taking information from the imported pokemonArray
 //And making the necessary html for each, applying the corresponding stylings from the styles.scss document
 const makePokemonCard = (pokemonArray: Pokemon) => {
-//used the above capitaliseFirstLetter to take the names from the array and create a new capitalisedPokemonName function.
+  //used the above capitaliseFirstLetter to take the names from the array and create a new capitalisedPokemonName function.
   const capitalisedPokemonName = capitaliseFirstLetter(pokemonArray.name);
   return `<div class="card">
                         <img
@@ -32,11 +39,11 @@ const makePokemonCard = (pokemonArray: Pokemon) => {
 
 const container = document.querySelector<HTMLElement>(".card-container");
 
-const pokemonSearch = document.querySelector<HTMLInputElement>(".pokemon_search")
-
 if (!container) {
   throw new Error("Issue with selector");
 }
 pokemonArray.forEach((Pokemon) => {
   container.innerHTML += makePokemonCard(Pokemon);
 });
+
+pokemonSearch?.addEventListener("input", filterBySearch);
