9669c11e1388d59d236252e5084a6b8190bb2979
callum <callumgibbons@icloud.com>
2024-04-04 17:20:50 +0530

    removed non-working code to publish work

--- pokedomTest-main/src/main.ts
+++ pokedomTest-main/src/main.ts
@@ -2,17 +2,11 @@ import { Pokemon } from "./data/types";
 import { pokemonArray } from "./data/pokemon";
 import "./styles/style.scss";
 
-const pokemonSearch = document.querySelector<HTMLInputElement>(".pokemon_search");
-
-
-const filterBySearch = pokemonArray.filter((pokemon) => `${pokemonSearch}` in pokemon)
-const filteredPokemon = filterBySearch.map((searchedPokemon) => searchedPokemon.pokemonSearch)
 //this takes each string in a given array and converts the first letter of each item to capital
 const capitaliseFirstLetter = (string: string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-console.log(filterBySearch);
 
 
 //created function for the making of each card, taking information from the imported pokemonArray
@@ -44,4 +38,3 @@ pokemonArray.forEach((Pokemon) => {
   container.innerHTML += makePokemonCard(Pokemon);
 });
 
-pokemonSearch?.addEventListener("input", filterBySearch);
